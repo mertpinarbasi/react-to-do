47d2b8fec3b2da934c41118b5826fc8201e46c7d
Mert Pınarbaşı <mpinarbasi35@gmail.com>
2022-08-04 22:04:37 +0300

    material ui is applied to todoItem and todoComponent and then dialog components is created for adding new todo

--- src/components/todo-component/todo.component.jsx
+++ src/components/todo-component/todo.component.jsx
@@ -1,26 +1,119 @@
 import React from "react";
-// import { useState } from 'react';
-
-const fakeList = [{ id: 1, taskContent: "todoItem1", taskStatus: "done" }, { id: 2, taskContent: "todoItem2", taskStatus: "Doing" }]
-
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import { Stack } from '@mui/material';
+import TextField from '@mui/material/TextField';
+import PropTypes from 'prop-types';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 import ToDoItem from '../todoItem-component/todoItem.component'
+
+const fakeList = [{ id: 1, taskTitle: "Item1", taskContent: "todoItem1", taskStatus: "done" }, { id: 2, taskTitle: "Item2", taskContent: "todoItem2", taskStatus: "Doing" }]
+
+
 function Todo() {
 
+  const [open, setOpen] = React.useState(false);
 
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+
+
+  };
   const listToDos = fakeList.map(todoItem =>
     <ToDoItem
       key={todoItem.id}
+      taskTitle={todoItem.taskTitle}
       taskContent={todoItem.taskContent}
       taskStatus={todoItem.taskStatus}>
     </ToDoItem>);
 
+
   return (
     <div className="ToDo">
-      {listToDos}
+      <AddTodoDialog open={open} onClose={handleClose}></AddTodoDialog>
+      <Card style={{ backgroundColor: "#DEF2F1" }}>
+        <CardContent>
+          {listToDos}
+        </CardContent>
+        <Stack
+          direction="row"
+          justifyContent="flex-end"
+          alignItems="center"
+          spacing={2}>
+
+
+
+          <CardActions>
+            <Button size="medium" variant="contained" color="primary" onClick={handleClickOpen}>Add Todo</Button>
+
+          </CardActions>
+        </Stack>
+      </Card>
     </div>
 
 
 
   )
+
+
 }
+
+function AddTodoDialog(props) {
+  const { onClose, open } = props;
+
+  const handleClose = () => {
+    onClose();
+  };
+
+
+
+
+  return (
+    <Dialog
+      maxWidth="lg"
+      fullWidth onClose={handleClose}
+      open={open}
+      scroll="body"
+    >
+      <DialogTitle>Add Todo</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          To subscribe to this website, please enter your email address here. We
+          will send updates occasionally.
+        </DialogContentText>
+
+        <TextField
+          autoFocus
+          margin="dense"
+          id="name"
+          label="Email Address"
+          type="email"
+          fullWidth
+          variant="standard"
+        />
+      </DialogContent>
+      <DialogActions>
+
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+AddTodoDialog.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  open: PropTypes.bool.isRequired,
+
+};
+
 export default Todo;
--- src/components/todoItem-component/todoItem.component.jsx
+++ src/components/todoItem-component/todoItem.component.jsx
@@ -1,22 +1,65 @@
-import React from "react";
+import * as React from 'react';
 import PropTypes from 'prop-types';
-
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import DeleteIcon from '@mui/icons-material/Delete';
+import AutoFixHighIcon from '@mui/icons-material/AutoFixHigh';
+import { Stack } from '@mui/material';
+import Chip from '@mui/material/Chip';
 
 TodoItem.propTypes =
 {
   id: PropTypes.number,
+  taskTitle: PropTypes.string,
   taskContent: PropTypes.string,
   taskStatus: PropTypes.string,
 }
 
 
-function TodoItem({ taskStatus, taskContent }) {
+function TodoItem({ taskTitle, taskStatus, taskContent }) {
   return (
+
     <div className="ToDoItem">
 
+      <Card variant='outlined' style={{ backgroundColor: "#FEFFFF" }}>
 
-      <p>{taskContent}</p>
-      <p>{taskStatus}</p>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {taskTitle}
+          </Typography>
+          <Typography gutterBottom variant="body" component="div">
+            {taskContent}
+          </Typography>
+
+        </CardContent>
+
+
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Chip label={taskStatus} />
+
+
+          <Stack
+            direction="row"
+            justifyContent="flex-end"
+            alignItems="flex-start"
+            spacing={2}>
+
+
+            <CardActions>
+
+              <Button size="small" style={{ backgroundColor: "#3AAFA9" }} variant="contained" endIcon={<AutoFixHighIcon />}>Modify</Button>
+              <Button size="small" color="error" variant="contained" endIcon={<DeleteIcon />} >Delete</Button>
+            </CardActions>
+          </Stack>
+        </Stack>
+      </Card>
     </div>
   )
 }
